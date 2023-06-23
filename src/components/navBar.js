c18fc18cb8c8fa47793f4d44c2ea94e4766eb2fb
Shantanu <shantanupathak01@gmail.com>
2023-06-24 04:07:16 +0530

    first commit

---
+++ src/components/navBar.js
@@ -0,0 +1,16 @@
+
+import './navBar.css'
+import myimg from "../images/menu.png"
+
+function Navbar() {
+  return (
+    <div className="nav_box">
+        <div className="m" >
+            <img src={myimg}  />
+        </div>
+     
+    </div>
+  )
+}
+
+export default Navbar;
