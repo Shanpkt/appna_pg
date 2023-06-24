e42ab68290543b3d37ba474667171febdf85bad9
Shantanu <shantanupathak01@gmail.com>
2023-06-24 06:34:57 +0530

    4th

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import Navbar from './components/navBar';
 import "./App.css"
+import Citybox from './components/city_area';
 function App() {
   return (
     <div className="App">
      <Navbar/>
+     <Citybox/>
     </div>
   );
 }
---
+++ src/components/city_area.js
@@ -0,0 +1,13 @@
+
+import "./city_area.css"
+
+
+function Citybox() {
+  return (
+    <div className="city_box">
+       
+    </div>
+  )
+}
+
+export default Citybox;
