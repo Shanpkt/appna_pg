2d6bfd44a6a5e06859020388af26e741788e3379
Shantanu <shantanupathak01@gmail.com>
2023-06-25 20:36:24 +0530

    4th

--- src/components/menu.js
+++ src/components/menu.js
@@ -11,7 +11,20 @@ function Menu({trriger,settrigger}) {
 
   return (
     <div  className={`menu_box ${trriger==true ? 'menu_box1' : ''}`} >
-        <div className='close_icon' onClick={handle_close} ><img src={close}/></div>
+        
+        <div className='close_icon' onClick={handle_close} ><img src={close}/>   </div>
+
+         <div className='menu_list'>
+            <h3>Home</h3>
+            <h3>Profile</h3>
+            <h3>Refferals</h3>
+            <h3>My PG</h3>
+
+             <h3>LogOut</h3>
+         </div>
+
+
+     
 
     </div>
   )
