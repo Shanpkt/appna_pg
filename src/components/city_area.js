5b2a7e93d4bc7dcad00d8a200bc46b52a6ff6fab
Shantanu <shantanupathak01@gmail.com>
2023-06-25 22:35:34 +0530

    4th

--- src/components/city_area.js
+++ src/components/city_area.js
@@ -1,11 +1,26 @@
 
 import "./city_area.css"
+import unisex from "../images/unisex.png"
 
 
 function Citybox() {
   return (
     <div className="city_box">
-        <div className="inner_box" ></div>
+        <div className="inner_box" >
+          
+          <div className="inner_box_first" >
+            <div className="inner_box_second" >
+
+              <h5><img src={unisex}/>unisex</h5>
+              <h5><img src={unisex}/>unisex</h5>
+              <h5><img src={unisex}/>unisex</h5>
+            </div>
+          </div>
+  
+ 
+
+        </div>
+        
        
     </div>
   )
