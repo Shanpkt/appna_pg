c5eaa1378ec78b96dc969f3cd51b1321791db49b
Shantanu <shantanupathak01@gmail.com>
2023-06-26 01:28:04 +0530

    4th

--- src/components/panel.js
+++ src/components/panel.js
@@ -2,12 +2,13 @@ import React from 'react'
 import "./panel.css"
 import home from "../images/home.png"
 import profile from "../images/profile.png"
+import owner from "../images/icons8-agent-64.png"
 function Panel() {
   return (
     <div className='panel_box'>
 
       <img src={home} />
-     
+      <div><img src={owner}/><h4>Owner</h4></div>
       <img src={profile} />
 
     </div>
