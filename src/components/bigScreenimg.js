5596cbe7b935c4e39ba11d1cebcbf645ecea9b73
Shantanu <shantanupathak01@gmail.com>
2023-06-28 00:29:08 +0530

    4th

--- src/components/bigScreenimg.js
+++ src/components/bigScreenimg.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import room from "../images/room.jpg"
 import "./bigScreenimg.css"
+import PgInfo from './pgInfo'
 
 function BigScreenimg() {
   return (
     <div className="bigimg_box">
         <img src={room} />
-
-
+        <PgInfo/> 
     </div>
   )
 }
--- src/components/pgInfo.js
+++ src/components/pgInfo.js
@@ -1,11 +1,22 @@
 import React from 'react'
 import "./pgInfo.css"
+import location from "../images/location.png"
 
 function PgInfo() {
   return (
     <div className='info_pg_box' >
+      <div className='info_main_box'>
+    
+ <div className='info_one'>
+ <h3>Bhrama Pg</h3>
+   <h5><img src={location} /><a>Laxmi Chowk</a></h5> 
+     </div>
+     <div className='info_two'>
+      <h5>UNISEX</h5>
 
+     </div>
 
+     </div>
 
     </div>
 
