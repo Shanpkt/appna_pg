f2ed00ad8eccc2db11af751e99ee02ae8fe5f04c
Shantanu <shantanupathak01@gmail.com>
2023-06-28 21:35:46 +0530

    4th

--- src/App.js
+++ src/App.js
@@ -14,50 +14,22 @@ import axios from 'axios';
 const opencage = require('opencage-api-client');
 function App() {
 
-  const [data,setdata]=useState([])
-  const [cords,setcords]=useState("")
-
-  useEffect(() => {
-    findlocation()
-  
-  }, [])
-
  
- async function findlocation(){
-
-    navigator.geolocation.getCurrentPosition((position)=>{
-   // console.log(position.coords);
- fet(position.coords)
-
-    })
-
-  }
-
-
- async function fet(e){
-
-    const response = await axios.get(
-      `https://api.opencagedata.com/geocode/v1/json?key=e866f37a27334a859dd083191709fd6f&q=${e.latitude}+${
-        e.longitude}`
-    ).then((e)=>{console.log(e.data.results[0].components);setdata(e.data.results[0].components)
-      })
-
-  }
   
   return (
-    <div className="App">
-      <h3>{data.country}</h3> 
-    <h3>state-{data.state}</h3>
-      <h3>dist.-{data.state_district}</h3>
-      <h3>suburb-{data.suburb}</h3>
+     <div className="App">
+    {/* //   <h3>{data.country}</h3> 
+    // <h3>state-{data.state}</h3>
+    //   <h3>dist.-{data.state_district}</h3>
+    //   <h3>suburb-{data.suburb}</h3> */}
 
     {/* <Selecepg/> */}
     {/* <BigScreenimg/> */}
-    {/* <Routes>
+     <Routes>
       <Route path='/' element={<Mainpage/>} />
       <Route path='/PGselect' element={<Selecepg/>} />
       <Route path='/PGdetail' element={<BigScreenimg/>} />
-    </Routes> */}
+    </Routes> 
      
     </div>
   );
--- src/components/navBar.js
+++ src/components/navBar.js
@@ -3,6 +3,8 @@ import './navBar.css'
 import myimg from "../images/menu.png"
 import search from "../images/search.png"
 import filter from "../images/filter.png"
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 
 function Navbar({trriger,settrigger,filtertrg,setfiltertrg}) {
 
@@ -21,11 +23,43 @@ function Navbar({trriger,settrigger,filtertrg,setfiltertrg}) {
 
   }
 
+  const [data,setdata]=useState([])
+  const [cords,setcords]=useState("")
+
+  useEffect(() => {
+    findlocation()
+  
+  }, [])
+
+ 
+ async function findlocation(){
+
+    navigator.geolocation.getCurrentPosition((position)=>{
+   // console.log(position.coords);
+ fet(position.coords)
+
+    })
+
+  }
+
+
+ async function fet(e){
+
+    const response = await axios.get(
+      `https://api.opencagedata.com/geocode/v1/json?key=e866f37a27334a859dd083191709fd6f&q=${e.latitude}+${
+        e.longitude}`
+    ).then((e)=>{console.log(e.data.results[0].components);setdata(e.data.results[0].components)
+      })
+
+  }
+
 
   return (
     <div className="nav_box">
         <div className="box" >
+          <div className='menu_box_loc'>
             <img onClick={hope}  src={myimg}  />
+           <h5>{data.state_district}</h5></div>
             <img onClick={filterset} src={filter}  />
            
         </div>
