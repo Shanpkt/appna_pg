f58c77268b779194d1b5cca8f53358984bc68510
Shantanu <shantanupathak01@gmail.com>
2023-06-28 19:05:51 +0530

    4th

--- src/App.js
+++ src/App.js
@@ -3,25 +3,55 @@ import Navbar from './components/navBar';
 import "./App.css"
 import Citybox from './components/city_area';
 import Menu from './components/menu';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Panel from './components/panel';
 import BigScreenimg from './components/bigScreenimg';
 import PgInfo from './components/pgInfo';
 import Selecepg from './molecules/selecepg';
 import { Routes ,Route} from 'react-router-dom';
 import Mainpage from './molecules/mainpage';
+import axios from 'axios';
+const opencage = require('opencage-api-client');
 function App() {
+
+  const [data,setdata]=useState([])
+
+  useEffect(() => {
+    findlocation()
+  
+  }, [])
+
  
+ async function findlocation(){
+
+  navigator.geolocation.getCurrentPosition((position)=>{
+    console.log(position)
+
+  })
+
+    const response = await axios.get(
+      `https://api.opencagedata.com/geocode/v1/json?key=e866f37a27334a859dd083191709fd6f&q=${18.6010879}+${
+        73.732113}`
+    ).then((e)=>{console.log(e.data.results[0].components);setdata(e.data.results[0].components)
+      })
+
+  }
+  
   return (
     <div className="App">
+      <h3>{data.country}</h3> 
+    <h3>state-{data.state}</h3>
+      <h3>dist.-{data.state_district}</h3>
+      <h3>suburb-{data.suburb}</h3>
+
       
     {/* <Selecepg/> */}
     {/* <BigScreenimg/> */}
-    <Routes>
+    {/* <Routes>
       <Route path='/' element={<Mainpage/>} />
       <Route path='/PGselect' element={<Selecepg/>} />
       <Route path='/PGdetail' element={<BigScreenimg/>} />
-    </Routes>
+    </Routes> */}
      
     </div>
   );
