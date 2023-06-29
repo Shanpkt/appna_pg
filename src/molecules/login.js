24b6713603b7c5b5cfc862e585541bc5f5189189
Shantanu <shantanupathak01@gmail.com>
2023-06-29 09:51:00 +0530

    4th

--- src/molecules/login.js
+++ src/molecules/login.js
@@ -1,17 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./login.css"
 import loginWomen from "../images/loginWomen.png"
 import google from "../images/icons8-google-100.png"
 
 function Login() {
+const [infodata,setinfodata]=useState()
+
+
+function login(e){
+const {name,value}=e.target
+  
+  setinfodata({
+    ...infodata,
+    [name]:value
+  })
+}
+
+console.log(infodata)
+
   return (
     <div className='login_main'>
         <div className='white_curtain'>
         <div className='login_box' >
             <img src={loginWomen} />
             <div className='login_details_input'>
-                <input placeholder='Email' />
-                <input placeholder='Password' />
+                <input onChange={login} name='Email' placeholder='Email' />
+                <input onChange={login} name='Password' placeholder='Password' />
+                <button className='login_button' >Login</button>
             </div>
             <h6>dont have Account?<span> Signup</span></h6>
         </div>
