c8567516b2bfbd51df72d32f1c6aca1a5c0cbb00
Shantanu <shantanupathak01@gmail.com>
2023-07-13 00:57:33 +0530

    first commit

--- src/components/profilebottom.js
+++ src/components/profilebottom.js
@@ -3,6 +3,7 @@ import "./profilebottom.css"
 import setting from "../images/icons8-setting-100.png"
 import bulding from "../images/icons8-bulding-64.png"
 import patner from "../images/icons8-friend-64.png"
+import logout from "../images/icons8-logout-100.png"
 
 function Profilebottom() {
   return (
@@ -11,8 +12,9 @@ function Profilebottom() {
         <h4><img src={setting} />Setting</h4>
         <h4><img src={bulding} />My PG</h4>
         <h4><img src={patner} />Find Roomate</h4>
-        
-        <button>Log Out</button>
+       <div className='button_box' >
+        <button className='logout_button' >Log Out <img src={logout} /> </button>
+        </div> 
     </div>
   )
 }
